316caa3110128190fd089135f6b7a1719e7ec74a
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-11-18 21:53:25 +0300

    move database connection properites to properties file

--- src/main/java/UserGroups.java
+++ src/main/java/UserGroups.java
@@ -6,17 +6,27 @@ import repository.JdbcGroupRepository;
 import repository.JdbcUserRepository;
 import repository.UserRepository;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Properties;
 
 
 public class UserGroups {
     public static void main(String[] args) {
+
         try {
-            Connection connection = DriverManager.getConnection("jdbc:h2:C:/tt/embededBase","admin","");
+            Properties dbproperties = new Properties();
+            dbproperties.load(UserGroups.class.getResourceAsStream("/db/db.properties"));
+            Connection connection = DriverManager.getConnection(
+                    (String)dbproperties.get("url"),
+                    (String)dbproperties.get("username"),
+                    (String)dbproperties.get("password"));
             GroupRepository groupRepository = new JdbcGroupRepository(connection);
-            //groupRepository.save(new Group(2, "222"));
             List<Group> groups = groupRepository.getGroupsWithMoreThenTwoUsers();
             UserRepository userRepository = new JdbcUserRepository(connection);
             User user = new User("UserX", "UserX", Status.Active, LocalDate.of(1980, 8, 6),groups.get(0));
@@ -27,6 +37,8 @@ public class UserGroups {
             userRepository.setStatus(user, Status.Inactive);
             userRepository.getUsersByGroupAndStatus(groups.get(0),Status.Active).stream().forEach(System.out::println);
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
